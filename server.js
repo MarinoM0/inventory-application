9ad2dd69c2a6386f106f705dd75967109fa79998
MarinoM0 <161130926+MarinoM0@users.noreply.github.com>
2025-12-07 17:44:13 +0100

    Create folders for routes/controllers/models

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require("express");
 
 const app = express();
 
+const categoriesRoutes = require("./routes/categoriesRoutes");
+const itemsRoutes = require("./routes/itemsRoutes");
+
 app.get("/", (req, res) => {
   res.send("app running");
 });
 
+app.use("/categories", categoriesRoutes);
+app.use("/items", itemsRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log/(`Server listening on http://localhost:${PORT}`)
-})
+  console.log / `Server listening on http://localhost:${PORT}`;
+});
